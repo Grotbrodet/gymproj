64dbc915c5dab2672adc512b46f80e367c60934a
Grotbrodet <harmygyr@gmail.com>
2024-09-03 20:24:30 +0200

    repo1TestNbase

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleSwitch(button) {
+    if (button.style.backgroundColor === 'red') {
+        button.style.backgroundColor = 'white';
+    } else {
+        button.style.backgroundColor = 'red';
+    }
+}
